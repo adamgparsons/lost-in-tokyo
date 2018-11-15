4e6873c038bc7a90c8f010421424f454e1d6dffc
Adam <adam@Adams-MacBook-Air.local>
2018-11-15 10:58:27 +0000

    Progress up to 25/28

--- js/app.js
+++ js/app.js
@@ -1,48 +1,92 @@
-const Highlight = props => (
-  <span className={`relative highlight highlight-${props.color}`}>
-    <span className="relative z-2">{props.children}</span>
+const Highlight = ({color, children}) => (
+  <span className={`relative highlight highlight-${color}`}>
+    <span className="relative z-2"> {children} </span>{' '}
   </span>
 );
 
 const Intro = () => (
   <div className="m-auto-ns f4 f3-m f2-1 tc w-80-l normal">
     <div className="mb3 mb4-ns">
-      <Highlight color="aqua">Lost in Tokyo</Highlight> is a directory of fun
-      places to see, play in and <Highlight color="yellow">explore</Highlight>,
-      in <Highlight color="blue">Tokyo</Highlight>, Japan.{" "}
-    </div>
+      <Highlight color="aqua"> Lost in Tokyo </Highlight> is a directory of fun places to see, play
+      in and <Highlight color="yellow"> explore </Highlight>, in{' '}
+      <Highlight color="blue"> Tokyo </Highlight>, Japan.{' '}
+    </div>{' '}
     <div>
-      From <Highlight color="blue">museums</Highlight> and{" "}
-      <Highlight color="blue">galleries</Highlight>, to{" "}
-      <Highlight color="pink">robot restaurants</Highlight> and{" "}
-      <Highlight color="pink">kitten cafes</Highlight>. Tokyo is the gift that
-      keeps on giving. <Highlight color="yellow">Dattebayo!</Highlight>
-    </div>
+      From <Highlight color="blue"> museums </Highlight> and{' '}
+      <Highlight color="blue"> galleries </Highlight>, to{' '}
+      <Highlight color="pink"> robot restaurants </Highlight> and{' '}
+      <Highlight color="pink"> kitten cafes </Highlight>. Tokyo is the gift that keeps on giving.{' '}
+      <Highlight color="yellow"> Dattebayo! </Highlight>{' '}
+    </div>{' '}
   </div>
+);
+
+const NavItem = ({className, href, children, logo}) => (
+  <li className={`mh2-ns f6 f4-l tc ${className}`}>
+    <a className="white no-underline" href={href}>
+      {logo ? <img src="../images/logo.svg" className="db center logo" /> : children}
+    </a>
+  </li>
 );
 
 const Nav = () => (
   <nav className="pt3 pt4-ns mb4 mb0-ns">
     <ul className="list flex flex-wrap flex-nowrap-ns justify-between items-center pa0 ma0">
       {menu.map(item => (
-        <li className={`mh2-ns f6 f4-l tc ${item.className}`}>
-          <a className="white no-underline" href={item.href}>
-            {item.children}
-          </a>
-        </li>
-      ))}
-    </ul>
+        <NavItem {...item} />
+      ))}{' '}
+    </ul>{' '}
   </nav>
 );
+
+class Attraction extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showInfo: false
+    };
+  }
+
+  render() {
+    const {title, description, className, image} = this.props;
+    const {showInfo} = this.state;
+    return (
+      <div
+        className={`ph4 ph5-ns ph0-l mb4 mb5-ns w-100 overflow-hidden pointer attraction ${className}`}
+        onClick={() => this.setState({showInfo: true})}
+      >
+        <div className="relative">
+          <div
+            className="absolute w-100 h-100 flex items-center pa3 pa4-ns bg-aqua overlay"
+            style={{
+              // we do a test to see whether showInfo is true
+              transform: showInfo ? 'none' : 'translateY(-100%)'
+            }}
+          >
+            <div>
+              <h1 className="f4 f3-ns mt0 mb2 regular black normal lh-title">{title}</h1>
+              <p className="lh-title lh-copy-ns mv0 black f6 measure-l">{description}</p>
+            </div>
+          </div>
+          <img src={`../images/${image}`} className="db" />
+        </div>
+      </div>
+    );
+  }
+}
 
 const App = () => (
   <div>
     <Nav />
     <div className="min-vh-100 ph4 flex flex-column">
       <Intro />
+    </div>{' '}
+    <div className="flex flex-wrap container">
+      {attractions.map(item => (
+        <Attraction {...item} />
+      ))}
     </div>
-    <div className="flex flex-wrap container" /> /* attactions list component */
   </div>
 );
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
